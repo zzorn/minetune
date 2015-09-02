b339f22df03e70d93e967a5cfb7c8382694d0554
zzorn <zzorn@iki.fi>
2015-09-02 17:36:13 +0300

    Setup build

---
+++ src/main/java/org/minetune/MineTune.java
@@ -0,0 +1,15 @@
+package org.minetune;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ */
+public class MineTune extends JavaPlugin {
+
+    @Override public void onEnable() {
+    }
+
+    @Override public void onDisable() {
+    }
+}
